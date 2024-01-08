6dba390cf3b93ddbd84078f9dcea9b8832df1971
Kaditya67 <322adityakumar0004@dbit.in>
2024-01-08 20:34:47 +0530

    Double_LinkedList using C++

---
+++ Double_LinkedList.cpp
@@ -0,0 +1,280 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+private:
+    int data;
+    Node* next;
+    Node* prev;
+
+public:
+    // Constructor to initialize a node with a given value
+    Node(int value) : data(value), next(nullptr) {}
+
+    // Getter for the next pointer
+    Node*& getNext() {
+        return next;
+    }
+    // Getter for the prev pointer
+    Node*& getPrev() {
+        return prev;
+    }
+
+    // Getter for the data value
+    int getData() const {
+        return data;
+    }
+};
+
+class LinkedList {   // -....<-Node2<-Node1<-head
+private:
+    Node* head;  // Head of the linked list
+
+public:
+    // Constructor to initialize an empty linked list
+    LinkedList() : head(nullptr) {}
+
+    // Function to append an element at the end of the linked list
+    void append(int value);
+
+    // ----------- at the beginning -----------
+    void appendAtBeg(int value);
+
+    // -------- at a specific position---------
+    void appendAtPosition(int position, int value);
+
+    // Function to delete the first element of the linked list
+    void deleteAtBeg();
+
+    // ------- last element-----------
+    void deleteAtEnd();
+
+    // --------- at a specific position --------------
+    void deleteAt(int position);
+
+    // --------Display the elements of the linked list
+    void display() const;
+
+    // Destructor to free memory allocated for nodes
+    ~LinkedList();
+};
+
+void LinkedList::append(int value) {
+    Node* newNode = new Node(value);
+
+    if (head == nullptr) {
+        // If the list is empty, make the new node the head and link it to itself
+        head = newNode;
+        newNode->getPrev() = head;
+    } else {
+        // Traverse to the end and link the last node to the new node
+        Node* current = head;
+        while (current->getNext() != nullptr) {
+            current = current->getNext();
+        }
+        current->getNext() = newNode;
+        newNode->getPrev() = current;  // Corrected line
+    }
+}
+
+void LinkedList::appendAtBeg(int value) {
+    Node* newNode = new Node(value);
+
+    if (head == nullptr) {
+        // If the list is empty, make the new node the head
+        head = newNode;
+    } else {
+        // Link the new node to the current head and update the head
+        newNode->getNext() = head;
+        head->getPrev() = newNode;
+        head = newNode;
+    }
+}
+
+void LinkedList::appendAtPosition(int position, int value) {
+    if (position < 1) {
+        cout << "Invalid position. Position should be greater than or equal to 1.\n";
+        return;
+    }
+
+    Node* newNode = new Node(value);
+
+    if (position == 1) {
+        // If position is 1, insert at the beginning
+        newNode->getNext() = head;
+        head->getPrev() = newNode;
+        head = newNode;
+        return;
+    }
+
+    // Traverse to the position - 1 and insert the new node
+    Node* temp = head;
+    for (int i = 1; i < position - 1 && temp != nullptr; i++) {
+        temp = temp->getNext();
+    }
+
+    if (temp == nullptr) {
+        cout << "Invalid position. Position exceeds the size of the list.\n";
+        return;
+    }
+    newNode->getNext() = temp->getNext();
+    newNode->getPrev() = temp;  
+    temp->getNext() = newNode;
+}
+
+void LinkedList::display() const {
+    if (head == nullptr) {
+        cout << "Linked List is empty.\n";
+        return;
+    }
+
+    // Traverse and display each element in the linked list
+    Node* current = head;
+    while (current != nullptr) {
+        cout << current->getData() << " ";
+        current = current->getNext();
+    }
+    cout << endl;
+}
+
+void LinkedList::deleteAtBeg() {
+    if (head == nullptr) {
+        cout << "Linked List is empty. Nothing to delete.\n";
+        return;
+    }
+
+    // Move head to the next node and delete the previous head
+    Node* temp = head;
+    head = head->getNext();
+    delete temp;
+}
+
+void LinkedList::deleteAtEnd() {
+    if (head == nullptr) {
+        cout << "Linked List is empty. Nothing to delete.\n";
+        return;
+    }
+
+    // If there is only one node, delete it and set head to null
+    if (head->getNext() == nullptr) {
+        delete head;
+        head = nullptr;
+        return;
+    }
+
+    // Traverse to the second-to-last node
+    Node* current = head;
+    while (current->getNext()->getNext() != nullptr) {
+        current = current->getNext();
+    }
+
+    // Delete the last node and update the link
+    delete current->getNext();
+    current->getNext() = nullptr;
+}
+
+void LinkedList::deleteAt(int position) {
+    if (head == nullptr) {
+        cout << "Linked List is empty. Nothing to delete.\n";
+        return;
+    }
+
+    if (position == 1) {
+        // If position is 1, delete at the beginning
+        Node* temp = head;
+        head = head->getNext();
+        delete temp;
+        return;
+    }
+
+    // Traverse to the position - 1
+    Node* temp = head;
+    for (int i = 1; i < position - 1 && temp != nullptr; i++) {
+        temp = temp->getNext();
+    }
+
+    if (temp == nullptr || temp->getNext() == nullptr) {
+        cout << "Invalid position. Position exceeds the size of the list.\n";
+        return;
+    }
+
+    // Delete the node at the specified position and update the link
+    Node* deleteNode = temp->getNext();
+    temp->getNext() = deleteNode->getNext();
+    deleteNode->getNext()->getPrev()=temp;
+    delete deleteNode;
+}
+
+// Destructor to free memory allocated for nodes
+LinkedList::~LinkedList() {
+    while (head != nullptr) {
+        Node* temp = head;
+        head = head->getNext();
+        delete temp;
+    }
+}
+
+int main() {
+    LinkedList mylist;
+    int choice;
+
+    // Menu-driven program
+    do {
+        cout << "1. Append Element\n";
+        cout << "2. Append at Beginning\n";
+        cout << "3. Append at Position\n";
+        cout << "4. Display\n";
+        cout << "5. Exit\n";
+        cout << "6. Delete at Beginning\n";
+        cout << "7. Delete at End\n";
+        cout << "8. Delete at Position\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                int value;
+                cout << "Enter the value to append: ";
+                cin >> value;
+                mylist.append(value);
+                break;
+            case 2:
+                int begValue;
+                cout << "Enter the value to append at the beginning: ";
+                cin >> begValue;
+                mylist.appendAtBeg(begValue);
+                break;
+            case 3:
+                int position, posValue;
+                cout << "Enter the position to append: ";
+                cin >> position;
+                cout << "Enter the value to append at position " << position << ": ";
+                cin >> posValue;
+                mylist.appendAtPosition(position, posValue);
+                break;
+            case 4:
+                mylist.display();
+                break;
+            case 5:
+                cout << "Exiting program.\n";
+                break;
+            case 6:
+                mylist.deleteAtBeg();
+                break;
+            case 7:
+                mylist.deleteAtEnd();
+                break;
+            case 8:
+                int deletePosition;
+                cout << "Enter the position to delete: ";
+                cin >> deletePosition;
+                mylist.deleteAt(deletePosition);
+                break;
+            default:
+                cout << "Invalid choice. Please try again.\n";
+        }
+
+    } while (choice != 5);
+
+    return 0;
+}
